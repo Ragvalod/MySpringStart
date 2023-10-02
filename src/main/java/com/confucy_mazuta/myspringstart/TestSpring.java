772a034831f00eb75d1d4295bd73b27defcab9b5
Ragvalod <konfucy@gmail.com>
2023-10-02 21:39:43 +0300

    Начало

--- src/main/java/com/confucy_mazuta/myspringstart/TestSpring.java
+++ src/main/java/com/confucy_mazuta/myspringstart/TestSpring.java
@@ -1,19 +1,4 @@
 package com.confucy_mazuta.myspringstart;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class TestSpring {
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
-
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-
-        System.out.println(testBean.getName());
-        testBean.setName("Хуйлан!");
-        System.out.println(testBean.getName());
-
-        context.close();
-    }
 }
